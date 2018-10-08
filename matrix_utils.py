e0990202db5b9df00c32063e6b7571c42b78ec7e
Nick Buoncristiani <nick.buoncristiani@berkeley.edu>
2018-10-08 11:28:44 -0700

    Added get_rankings function to matrix_utils

---
+++ matrix_utils.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+#Pass in eigenvals/vectors and receive corresponding rankings.
+def get_rankings(eig_vals, eig_vectors):
+	eig_one_arr = list(map(basically_one, eig_vals))
+	assert True in eig_one_arr
+	return eig_vectors[:,eig_one_arr.index(True)]
+
+#To handle float precision.
+def basically_one(n):
+	return abs(n - 1) < .000000000000001
+
+
