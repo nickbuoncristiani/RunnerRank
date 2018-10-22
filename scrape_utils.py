9b6c5f3696b4ac7b2b4f5a913be4bb2fd2768a70
Nick Buoncristiani <nick.buoncristiani@berkeley.edu>
2018-10-22 02:31:12 -0700

    adding graph functionality. should we even use networkx?

--- scrape_utils.py
+++ scrape_utils.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen, Request
 import re, Athlete
+import networkx as nx
 
 #Uses raw re to extract race data from url.
 def process_race(race_url, data_graph):
@@ -10,18 +11,32 @@ def process_race(race_url, data_graph):
 
 	results = pattern.findall(race_info)
 
+	surpassers = []
 	for result in results:
-		process_athlete_result(result, data_graph)
+		athlete = process_athlete_result(result, data_graph)
+		for surpasser in surpassers:
+			data_graph.add_edge(athlete, surpasser, 1)
+		surpassers.append(athlete)
 
 #processes a single match found by regular expression, adding data to graph.
 def process_athlete_result(result_data, data_graph):
 	pattern = re.compile(r'(null|true|false)')
 	result_data = re.sub(pattern, 'None', result_data)
-	result = eval(result_data)
+	result = eval(result_data)  
 	a = Athlete.Athlete(result['FirstName'] + ' ' + result['LastName'], result['AthleteID'])
-	#print(a)
-	#print('-----\n')
+	if a not in data_graph:
+		data_graph.add_node(a)
+	return a
 
 if __name__ == "__main__":
 	race_url = 'https://www.athletic.net/CrossCountry/meet/154540/results/644131'
-	process_race(race_url, 'a')
+	g = nx.MultiDiGraph()
+	process_race(race_url, g)
+	process_race(race_url, g)
+	process_race(race_url, g)
+	"""for athlete in g:
+		print(g.out_degree(athlete, weight='weight'))
+		print('\n') """
+	a = Athlete.Athlete('asdf', 9126976)
+	b = Athlete.Athlete('ffsa', 12401545)
+	print(g[b][a])
