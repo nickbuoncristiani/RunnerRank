2fe8d9661fc97431c01dd0a4a6509ae06b1e9bf0
Nick Buoncristiani <nick.buoncristiani@berkeley.edu>
2018-10-22 16:55:32 -0700

    added importer.py

---
+++ importer.py
@@ -0,0 +1,5 @@
+import scrape_utils.py
+
+#Scrapes starting from athlete_ids, updates date_graph and adds all new athletes to athletes set
+def importer(races_seen, athletes_by_id, data_graph, *athlete_ids):
+	
