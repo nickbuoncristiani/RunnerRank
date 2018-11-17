54d256141dda7834d7af0cc4db5ed688b64883c6
Nick Buoncristiani <nick.buoncristiani@berkeley.edu>
2018-11-17 10:12:27 -0800

    asdf' '

---
+++ race_scraper.py
@@ -0,0 +1,64 @@
+from urllib.request import urlopen, Request
+import re, Athlete, time, Meet
+import Save
+import datetime as Date
+
+RESULT_PATTERN = re.compile(r'{"Result":[^}]+}')
+DATE_PATTERN = re.compile(r'"MeetDate":\"\d{4}.\d{2}.\d{2}')
+
+#Uses raw re to extract race data from url.
+def process_race(race_url):
+	print('working on race: ' + race_url)
+	time.sleep(5)
+	try:
+		req = Request(race_url, headers={'User-Agent': 'Mozilla/5.0'})
+		with urlopen(req) as page:
+			race_info = str(page.read())
+	except:
+		print("Error handling race: " + race_url)
+		time.sleep(120)
+		process_race(race_url, save, queue, new_athletes_to_add)
+
+	results = re.findall(RESULT_PATTERN, race_info)
+	race_date = re.search(DATE_PATTERN, race_info).group()
+	str_date = re.split(':', race_date)[1]
+	date = process_date(str_date)
+
+	meet_name = re.search('style="cursor:pointer;">[^<]+<', race_info).group()
+	meet_name = re.findall('>[^<]+', meet_name)[0][1:]
+
+	for result, place in zip(results, range(1, len(results) + 1)):
+		current_athlete, time = process_athlete_result(result, meet_name, save) 
+		if not(current_athlete): 
+			continue
+		finish_results[current_athlete] = (place, time)
+		
+	meet = Meet(meet_name, date, race_url, finished_results)
+	for athlete in meet.results:
+		athlete.add_race(meet)
+
+#processes a single match found by regular expression, adding data to save
+def process_athlete_result(result_data):
+	pattern = re.compile(r'(null|true|false)')
+	result_data = re.sub(pattern, 'None', result_data) 
+	try:
+		result = eval(result_data)
+		time = result['SortValue']
+		a_id = result['AthleteID']
+		name = result['FirstName'] + ' ' + result['LastName']
+		assert time is not None and a_id is not None
+		athlete = Athlete(a_id, name)
+	except:
+		return None
+	return athlete, time
+
+#takes date string and returns appropriate datetime object
+def process_date(date_string):
+	date_list = re.split(r'\D', date_string)[1:]
+	date_ints = list(map(int, date_list))
+	year, month, day = date_ints
+	race_date = Date.datetime(year, month, day)
+	return race_date
+
+if __name__ == "__main__":
+	print(5)
